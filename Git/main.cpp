836ceca30d4c6cd01de179c8321b6ee99900794b
Iurii Tikhonov <zeffirkins@gmail.com>
2022-04-29 06:46:32 +0300

    add new message in Git project

--- Git/main.cpp
+++ Git/main.cpp
@@ -2,8 +2,6 @@
 
 int main() {
     std::cout << "Hello World";
-    std::cout << "How are you?\n";
-    std::cout << "i'm fine)\n";
 
     std::cout << "This message create for push to github =)\n";
 }
